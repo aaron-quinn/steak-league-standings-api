1be88cdafd95f5ad4a1c281d115b6bdb60dc4e81
Aaron Quinn <awquinn33@gmail.com>
2022-09-12 23:22:33 -0500

    Reverting

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ fastify.register(routes);
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    await fastify.listen({ port: 3000 });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
