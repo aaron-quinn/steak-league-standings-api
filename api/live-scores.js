ec7a9ef0ee9e77059ff661432ca2760cbfd55207
Aaron Quinn <awquinn33@gmail.com>
2023-01-08 19:42:03 -0600

    Fixed week 18 bug
    Related to there not being any matchups on the schedule

--- api/live-scores.js
+++ api/live-scores.js
@@ -5,24 +5,24 @@ export default async function getLiveScores({ season, leagueID, prefix = '' }) {
     // Get the live scores from the MFL API
     const liveScoresURL = `/${season}/export?TYPE=liveScoring&L=${leagueID}&JSON=1`;
 
-    console.log(liveScoresURL);
     const liveScoresResponse = await getData(liveScoresURL);
     const matchups = liveScoresResponse.liveScoring.matchup;
     const teamsOnBye = liveScoresResponse.liveScoring.franchise;
 
     const liveScores = {};
 
-    matchups.forEach((matchup) => {
+    (matchups || []).forEach((matchup) => {
       const matchupTeams = matchup.franchise;
       matchupTeams.forEach((team) => {
         const { id, score } = team;
         liveScores[`${prefix}${id}`] = score;
       });
     });
-    teamsOnBye.forEach((teamOnBye) => {
+    (teamsOnBye || []).forEach((teamOnBye) => {
       const { id, score } = teamOnBye;
       liveScores[`${prefix}${id}`] = score;
     });
+
     return liveScores;
   } catch (error) {
     return { error };
